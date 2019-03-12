c485eec83b015787fdc110cad04a6da7d5c971ae
Bang Nguyen <bangnguyen@motorola.com>
2019-03-12 08:18:12 -0500

    disp: msm: Register for reboot_notifier
    Since implementing the factory_kill feature, then the msm_drv needs
    to register for reboot_notifier to shut down the display accordingly.
    
    Port from 7445d2d.
    
    Change-Id: I8b6d48205ba819f0d36ad7218852241baa33f014
    Signed-off-by: Bang Nguyen <bangnguyen@motorola.com>
    Reviewed-on: https://gerrit.mot.com/1697588
    SLTApproved: Slta Waiver
    SME-Granted: SME Approvals Granted
    Tested-by: Jira Key
    Reviewed-by: Guobin Zhang <zhanggb@motorola.com>
    Submit-Approved: Jira Key

--- qcom/opensource/display-drivers/msm/msm_drv.h
+++ qcom/opensource/display-drivers/msm/msm_drv.h
@@ -39,6 +39,7 @@
 #include <linux/sizes.h>
 #include <linux/kthread.h>
 #include <linux/version.h>
+#include <linux/notifier.h>
 
 #include <drm/drm_atomic.h>
 #include <drm/drm_atomic_helper.h>
@@ -1113,6 +1114,7 @@ struct msm_drm_private {
 
 	struct mutex vm_client_lock;
 	struct list_head vm_client_list;
+	struct notifier_block msm_drv_notifier;
 };
 
 /* get struct msm_kms * from drm_device * */
