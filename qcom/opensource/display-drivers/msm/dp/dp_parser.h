767f0e2c58d0a187c740b7015e24022fe744f487
Konstantin Makariev <hcv867@motorola.com>
2021-11-05 10:22:38 -0500

    msm/dp: typec bridge option
    Add integration path to allow dp_altmode driver to hook up
    with either altmode-glink or with typec altmode displayport.
    
    This allows MSM DP display driver to work with 3rd party PD
    controllers managed by generic TypeC Policy Manager (tcpm).
    
    Change-Id: I9ba294138665377372ad9d45f0b84a0f2729f3c5
    Signed-off-by: Konstantin Makariev <hcv867@motorola.com>
    Reviewed-on: https://gerrit.mot.com/2108951
    SLTApproved: Slta Waiver
    SME-Granted: SME Approvals Granted
    Tested-by: Jira Key
    Reviewed-by: Igor Kovalenko <igork@motorola.com>
    Submit-Approved: Jira Key

--- qcom/opensource/display-drivers/msm/dp/dp_parser.h
+++ qcom/opensource/display-drivers/msm/dp/dp_parser.h
@@ -228,6 +228,7 @@ struct dp_parser {
 	bool dsc_continuous_pps;
 	bool has_widebus;
 	bool gpio_aux_switch;
+	bool typec_bridge;
 	u32 mst_fixed_port[MAX_DP_MST_STREAMS];
 	u32 qos_cpu_mask;
 	unsigned long qos_cpu_latency;
