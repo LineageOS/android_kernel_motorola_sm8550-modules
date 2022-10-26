767e6413876707edc7be3fcadbb226649a466691
huqian4 <huqian4@motorola.com>
2022-10-26 11:09:49 +0800

    Canyon: Remove MMI_STOPSHIP for user build
    details:
    For rtwo keep cci_debug_inf,other useless product remove.
    
    Change-Id: If005fa743e34ed3f966fc15a4087d3946dc8192e
    Signed-off-by: huqian4 <huqian4@motorola.com>
    Reviewed-on: https://gerrit.mot.com/2413690
    SME-Granted: SME Approvals Granted
    SLTApproved: Slta Waiver
    Tested-by: Jira Key
    Reviewed-by: <hezheng1@motorola.com>
    Reviewed-by: Long Cheng <chengl1@motorola.com>
    Reviewed-by: <liuzy55@motorola.com>
    Reviewed-by: Zhichao Chen <chenzc2@motorola.com>
    Submit-Approved: Jira Key

--- config/hiphi.mk
+++ config/hiphi.mk
@@ -1,14 +1,11 @@
 # Settings for compiling waipio camera architecture
 
 # Localized KCONFIG settings
-# MMI_STOPSHIP Camera: Remove for user build
-CONFIG_CCI_DEBUG_INTF := y
 
 CONFIG_CAM_SENSOR_PROBE_DEBUG := y
 
 CONFIG_CAM_CSI_CONDITIONAL_AUX := y
 
 # Flags to pass into C preprocessor
-ccflags-y += -DCONFIG_CCI_DEBUG_INTF=1
 ccflags-y += -DCONFIG_CAM_SENSOR_PROBE_DEBUG=1
 ccflags-y += -DCONFIG_CAM_CSI_CONDITIONAL_AUX=1
--- config/rtwo_factory.mk
+++ config/rtwo_factory.mk
@@ -1,7 +1,7 @@
 # Settings for compiling kalama camera architecture
 
 # Localized KCONFIG settings
-# MMI_STOPSHIP Camera: Remove for user build
+# Camera: Remove for user build
 CONFIG_CCI_DEBUG_INTF := y
 CONFIG_CAM_SENSOR_PROBE_DEBUG := y
 
