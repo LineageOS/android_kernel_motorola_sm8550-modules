ad24124f16bb864fcc37874c6bc8ef7fd4199103
wangdp11 <wangdp11@motorola.com>
2023-07-17 20:33:32 +0800

    ctwo: Enable CCI debug interface
    Detail:
    Enable CCI debug interface for UD and factory version.
    
    Change-Id: Ic1415a3aec9e5f5cedfb51c1b8bd05550938bf70
    Signed-off-by: wangdp11 <wangdp11@motorola.com>
    Reviewed-on: https://gerrit.mot.com/2677841
    SME-Granted: SME Approvals Granted
    SLTApproved: Slta Waiver
    Tested-by: Jira Key
    Reviewed-by: Pengfei Qiao <qiaopf1@motorola.com>
    Reviewed-by: Shuo Wang <wangsh18@motorola.com>
    Reviewed-by: <liuzy55@motorola.com>
    Reviewed-by: Zhichao Chen <chenzc2@motorola.com>
    Submit-Approved: Jira Key

--- config/ctwo.mk
+++ config/ctwo.mk
@@ -1,6 +1,10 @@
 # Settings for compiling kalama camera architecture
 
 # Localized KCONFIG settings
+ifneq (,$(filter userdebug eng, $(TARGET_BUILD_VARIANT)))
+CONFIG_CCI_DEBUG_INTF := y
+ccflags-y += -DCONFIG_CCI_DEBUG_INTF=1
+endif
 CONFIG_MOT_OIS_SEM1217S_DRIVER := y
 
 # Flags to pass into C preprocessor
--- config/ctwo_factory.mk
+++ config/ctwo_factory.mk
@@ -2,6 +2,8 @@
 
 # Localized KCONFIG settings
 CONFIG_MOT_OIS_SEM1217S_DRIVER := y
+CONFIG_CCI_DEBUG_INTF := y
 
 # Flags to pass into C preprocessor
 ccflags-y += -DCONFIG_MOT_OIS_SEM1217S_DRIVER=1
+ccflags-y += -DCONFIG_CCI_DEBUG_INTF=1
