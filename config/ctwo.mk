68c482d3a80a7a4a92e2ec2c3b6b2f942f1dcf68
liuwang2 <liuwang2@lenovo.com>
2023-08-07 15:25:01 +0800

    ctwo: enable CONFIG_MOT_OIS_AFTER_SALES_SERVICE
    [detail]
    enable CONFIG_MOT_OIS_AFTER_SALES_SERVICE for sem1217s OIS
    
    Change-Id: Ie414d66595f4fd45f3101dea3bbadcf152c79593
    Signed-off-by: liuwang2 <liuwang2@lenovo.com>
    Reviewed-on: https://gerrit.mot.com/2702101
    SME-Granted: SME Approvals Granted
    SLTApproved: Slta Waiver
    Tested-by: Jira Key
    Reviewed-by: <shihl5@motorola.com>
    Reviewed-by: <liuzy55@motorola.com>
    Reviewed-by: Zhuoran Xu <xuzr3@motorola.com>
    Submit-Approved: Jira Key

--- config/ctwo.mk
+++ config/ctwo.mk
@@ -11,3 +11,4 @@ CONFIG_CAM_SENSOR_PROBE_DEBUG := y
 # Flags to pass into C preprocessor
 ccflags-y += -DCONFIG_MOT_OIS_SEM1217S_DRIVER=1
 ccflags-y += -DCONFIG_CAM_SENSOR_PROBE_DEBUG=1
+ccflags-y += -DCONFIG_MOT_OIS_AFTER_SALES_SERVICE=1
