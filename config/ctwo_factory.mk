61c5632add62b46d0f2047441681d550e1205bf0
Xiaofeng Wang <wangxf32@lenovo.com>
2023-08-08 14:57:17 +0800

    ctwo: add retries for sensor id matching
    details:
    enable CONFIG_CAM_SENSOR_PROBE_DEBUG
    
    Change-Id: Ia1e24e93198dff176e4489c75dc79fed364ca1be
    Signed-off-by: Xiaofeng Wang <wangxf32@lenovo.com>
    Reviewed-on: https://gerrit.mot.com/2703472
    SME-Granted: SME Approvals Granted
    SLTApproved: Slta Waiver
    Tested-by: Jira Key
    Reviewed-by: Wang Liu <liuwang2@lenovo.com>
    Reviewed-by: Shuo Wang <wangsh18@motorola.com>
    Reviewed-by: <wangdp11@motorola.com>
    Reviewed-by: <liuzy55@motorola.com>
    Reviewed-by: Zhuoran Xu <xuzr3@motorola.com>
    Submit-Approved: Jira Key

--- config/ctwo_factory.mk
+++ config/ctwo_factory.mk
@@ -3,7 +3,9 @@
 # Localized KCONFIG settings
 CONFIG_MOT_OIS_SEM1217S_DRIVER := y
 CONFIG_CCI_DEBUG_INTF := y
+CONFIG_CAM_SENSOR_PROBE_DEBUG := y
 
 # Flags to pass into C preprocessor
 ccflags-y += -DCONFIG_MOT_OIS_SEM1217S_DRIVER=1
 ccflags-y += -DCONFIG_CCI_DEBUG_INTF=1
+ccflags-y += -DCONFIG_CAM_SENSOR_PROBE_DEBUG=1
